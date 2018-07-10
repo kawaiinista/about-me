784ac94c5df3070c36d4ac8448e1b660e1fe6c1f
kawaiinista <41088793+kawaiinista@users.noreply.github.com>
2018-07-10 15:15:35 -0700

    Create Hairco.js

---
+++ Hairco.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded",
+function() { // lagnuage is very similar to the one provided by code.org
+// only difference is you have to type it on your own but everything is very simalr or is exactly the same
+    alert("this is pretty cool!");
+    console.log("Jeuuuuuu");
+
+let myPhoto =
+document.getElementById("personal-image")
+myPhoto.addEventListener("click", changeImage())
+
+function changeImage(){
+console.log(myPhoto.src);
+if( myPhoto.src)
+
+}
+
+})
+
+
+
+
+
+
+}) // end of dom content loaded
